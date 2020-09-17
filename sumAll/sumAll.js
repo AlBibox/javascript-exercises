922b721f3214d62df8e1e1081b7e33bcda18f1f1
AlBibox <alberto.benedetto@rocketmail.com>
2020-09-17 15:24:29 +0200

    Added sumAll solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    if (typeof a != "number" || typeof b != "number") return "ERROR";
+    else{
+
+        let min = Math.min(a, b);
+        let max = Math.max(a, b);
+        let sum = 0;
+
+        for (i = min; i <= max; i++) {
+            sum += i;
+        }
+        if (sum < 0) return "ERROR";
+        else return sum;
+
+    }
+    
+
+    
 
 }
+console.log(sumAll(10,"5"));
 
 module.exports = sumAll
