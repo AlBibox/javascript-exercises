96a534ddef53beb545061e129ac288d8e3a894a3
AlBibox <alberto.benedetto@rocketmail.com>
2020-09-17 10:36:37 +0200

    Added Solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    let a = "";
+    if(n<0) return "ERROR";
+    else{
+        for (let i = 0; i < n; i++) {
+            a += string;
+
+        }
+        return a;
+
+    }
+    
 
 }
+
+console.log(repeatString("hey",3));
 
 module.exports = repeatString
