6e72f4949c3c369f4ea95d3d09fdf4f2669b73cd
AlBibox <alberto.benedetto@rocketmail.com>
2020-09-17 11:09:32 +0200

    added reverseString solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let a = [];
+    let b = string.split("");
 
+    for(let i=0; i<b.length;i++){
+
+        a.unshift(b[i]);   
+
+    }
+    return a.join("");
 }
+
+
 
 module.exports = reverseString
