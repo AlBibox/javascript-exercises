5ca2606d4c70f2630005f71eb742e42ccba17511
AlBibox <alberto.benedetto@rocketmail.com>
2020-10-06 12:41:13 +0200

    added calculator solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,56 @@
-function add () {
+function add (...args) {
+	let result = 0;
+	args.map(a => result += a); 
+	return result;
+}
+
+
+
+
+function subtract (a, ...args) {	
+	args.map(b => a -= b);
+	return a;
 	
 }
 
-function subtract () {
+function sum (arg) {
+	let result = 0;
+	arg.map(a => result += a);
+	return result;
 	
 }
 
-function sum () {
-	
+
+
+function multiply (args) {
+	let result = 1;
+	args.map(a => result *= a);
+	return result;	
 }
 
-function multiply () {
-	
+
+
+function power(a,b) {
+	let c = a;
+	for(let i = 1; i<b; i++){
+		c *= a;
+	}
+	return c;
 }
 
-function power() {
-	
+
+
+function factorial(x) {
+	 
+	let a = (x == 0 ? 1 : x);
+	for(let i = 1; i<x; i++ ){
+		a *= i;
+	}
+	return a;
 }
 
-function factorial() {
-	
-}
+console.log(factorial(5));
+
 
 module.exports = {
 	add,
