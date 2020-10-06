cc847d5fc634c7623f724c5ebdb09fc6b7e64a79
AlBibox <alberto.benedetto@rocketmail.com>
2020-10-06 13:48:53 +0200

    Added palindrome solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let a = (string.toLowerCase()).replace(/[^a-zA-Z0-9]/g, '');
+    let arr = a.split("");
+    let invArr = [];
+    arr.map(a => {invArr.unshift(a)});
+    let b = invArr.join("");
+    const isPalindrome = a == b ? true : false;
+    return isPalindrome;
+    
+    
+    
 }
 
 module.exports = palindromes
