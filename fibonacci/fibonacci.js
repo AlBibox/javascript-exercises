e3b996cbfd402a814d8f425d3c2424e84988617e
AlBibox <alberto.benedetto@rocketmail.com>
2020-10-07 11:45:45 +0200

    Added fibonacci sequence solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let arr = [1,1];
+    if(n <= 0){      
+        return "OOPS";
+    }else if(n > 2){
+        for(let i = 2; i < n; i++){
+            arr.push((arr[i-2])+(arr[i-1]));
+        }
+    }
 
+    
+    return arr[n-1];
 }
+
+console.log(fibonacci(10));
 
 module.exports = fibonacci
